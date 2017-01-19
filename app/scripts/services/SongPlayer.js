046da0409b7ce21e2f1eb71259d5f4ca8c8dfb9d
Jess Bird <jessannbird@gmail.com>
2017-01-19 08:34:33 -0700

    Assignment 7: Services Part 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,11 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
+        
+        /**
+        * @desc Holds the curently playing song object
+        * @type {Object}
+        */
         var currentSong = null;
         
         /**
@@ -29,24 +34,45 @@
             currentSong = song;
         }
         
+        /**
+        * @function playSong
+        * @desc Plays the current song using currentBuzzObject and sets the given song object's playing attribute to true
+        * @param {Object} song
+        */
+        vr playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+            // why diff from above here, and not currentSong.playing?
+        }        
         
+        
+        /**
+        * @function SongPlayer.play
+        * @desc Sets up and plays a new song or unpauses the current song when a play button is clicked
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
-                // why diff from above here, and not currentSong.playing?
+                playSong(song);
             }
             else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
+                    // had only the .play() line and not the song.playing = true line, but I think that's an error.
                 }
             }
         };
         
+        /**
+        * @function SongPlayer.pause
+        * @desc Pauses the currently playing song by pausing the currentBuzzObject and setting the given song object's playing attribute to false
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
+            // why pass in a song argument, and not just take for granted currentSong is already what is needed, like is done with currentBuzzObject?
         }
         
         return SongPlayer;
